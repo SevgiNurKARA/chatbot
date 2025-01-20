7be475886c410f4075b5fadd3f14bb5b0d2a9769
SevgiNurKARA <sevginurkara2002@gmail.com>
2025-01-20 07:14:35 +0300

    extraction method file added

--- input_extraction/input_extraction.py
+++ input_extraction/input_extraction.py
@@ -1,23 +1,19 @@
 import re
 import requests
 
-# 1. Adım: Abone numarasını çıkarma
 def extract_subscriber_number(input_text):
     match = re.search(r'\d+', input_text)
     if match:
-        return int(match.group(0))  # Numarayı tam sayı olarak döndür
+        return int(match.group(0)) 
     return None
-
-# 2. Adım: Fake API'ye istek gönderme (GET isteğiyle)
 def get_fake_api_data(subscriber_number):
     url = f"https://jsonplaceholder.typicode.com/posts/{subscriber_number}"
     try:
-        # API'den abone numarasına karşılık gelen veri almak için
         response = requests.get(url)
-        response.raise_for_status()  # HTTP hata kodları için exception fırlatır
-        data = response.json()  # JSON verisini alıyoruz
+        response.raise_for_status() 
+        data = response.json()  
         
-        # Veriyi kontrol etme
+        
         if 'body' in data:
             body = data.get("body", "Body bulunamadı")
             return body
@@ -26,7 +22,6 @@ def get_fake_api_data(subscriber_number):
     except requests.exceptions.RequestException as e:
         return f"API'ye bağlanılamadı: {e}"
 
-# 3. Adım: API cevabını işleme
 def get_output(input_text):
     subscriber_number = extract_subscriber_number(input_text)
     if subscriber_number:
@@ -36,5 +31,5 @@ def get_output(input_text):
         return "Abone numarası bulunamadı."
 
 # Örnek kullanım
-input_text = "Abone numaram 2"
+input_text = "Abone numaram 42"
 print(get_output(input_text))
